80e01fb75e845e309c71191084b832943de6964c
Kenneth Lim <klim314@gmail.com>
2018-09-08 15:21:27 +0800

    first working addlist

--- pingdoktor/urls.py
+++ pingdoktor/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("visitor/", views.Visitor.as_view()),
+    path("visitor/", views.Visitor.as_view(), name="visitor"),
     path(r"doctor/<int:doctor_id>", views.Doctor.as_view())
 ]
--- pingdoktor/views.py
+++ pingdoktor/views.py
@@ -9,7 +9,7 @@ class Visitor(View):
         """Display for the Visitor side
         """
         # revive from previous_state
-        current_visitors = models.Visitor.objects.filter(acknowledged=False)
+        current_visitors = models.Visitor.objects.filter(acknowledged=False).order_by("arrival_time")
         context = {"current_visitors": current_visitors}
         return render(request, "pingdoktor/visitor_home.html",
                       context=context)
@@ -19,12 +19,18 @@ class Visitor(View):
         Visitor information is then saved and signals set to websocket connections
         """
         # Extract parameters
-        params = request.post
+        params = request.POST
+        print(params)
         first_name = params["first_name"]
         last_name = params["last_name"]
+        description = params["description"]
+        print(first_name, last_name, description)
         # Validate and create visitor
-        services.add_visitor(first_name, last_name, ...)
-        return HttpResponse(json.dumps({"message": 1}))
+        visitor = services.add_visitor(first_name, last_name, description)
+        return HttpResponse(json.dumps({
+            "visitor_data": {"first_name": visitor.first_name,
+                             "last_name": visitor.last_name,
+                             "id": visitor.id}}))
 
 
 class Doctor(View):
